144b6cfc81d91e3639056b71fca1b3075b716dd6
TrevinN <tnance541@gmail.com>
2018-02-09 17:40:45 -0600

    trying to figure out user creation and verification

--- hi.php
+++
@@ -1,8 +0,0 @@
-<?php
-    #File: hi.php
-    function hello(){
-        echo "this worked";
-        echo "check if git works";
-    }
-    hello();
-?>
--- server/routes/auth.php
+++ server/routes/auth.php
@@ -4,6 +4,26 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 $app->post('/api/login', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write($request->getBody());
+    
+    require_once('../databases.php');
+    $username = $args['username'];
+    #encrypts the inputted password to compare with the stored one
+    $userPassword = hash('sha256', $args['password']);
+
+    #query which will return user 
+    #query which will return stored password
+    #$response->getBody()->write($request->getBody());
     return $response;
+});
+
+$app->put('/api/newUser', function (Request $request, Response $response, array $args){
+    
+    require_once('../databases.php');
+    $username = $args['username'];
+    #this line gets the password from args and encrypts it to store inDB
+    $userPassword = hash('sha256', $args['password']);
+
+    #do whatever to place the username in the table
+    #do whatever to put encrypted password in DB
+
 });

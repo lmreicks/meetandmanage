fd98aef0c840cecd00ef7fe593e0b207575def8b
TrevinN <tnance541@gmail.com>
2018-02-13 14:14:13 -0600

    changed user model to current version

--- server/controllers/auth.php
+++ server/controllers/auth.php
@@ -6,8 +6,7 @@ use Models\User;
 use logic\userCheck;
 
 $app->post('/api/login', function (Request $request, Response $response, array $args) {
-    
-    require_once('./logic/dbconnect.php');
+
     $body = $request->getParsedBody();
     #encrypts the inputted password to compare with the stored one
     $username = $body["username"];
@@ -29,23 +28,3 @@ $app->post('/api/login', function (Request $request, Response $response, array $
     return $response;
 });
 
-$app->post('/api/user', function (Request $request, Response $response, array $args){
-    
-    require_once('./logic/dbconnect.php');
-    $body = $request->getParsedBody();
-    $username = $body['username'];
-    #this line gets the password from args and encrypts it to store inDB
-    $userPassword = hash('sha256', $body['password']);
-    $user = new User;
-
-    $val = $user->validate($body);
-
-    $res = $user->save();
-    echo var_dump($val);
-    echo var_dump($res);
-
-    #do whatever to place the username in the table
-    #do whatever to put encrypted password in DB
-    #i changed someting
-    return $response;
-});
--- server/controllers/users.php
+++ server/controllers/users.php
@@ -12,3 +12,23 @@ $app->get('/api/user', function (Request $request, Response $response, array $ar
     $response->getBody()->write("get all users");
     return $response;
 });
+
+$app->post('/api/user', function (Request $request, Response $response, array $args){
+    
+    #user must have timezone ID made in table
+    $body = $request->getParsedBody();
+    $username = $body['username'];
+    #this line gets the password from args and encrypts it to store inDB
+    $userPassword = hash('sha256', $body['password']);
+    $user = new User;
+
+
+    $res = $user->save();
+    echo var_dump($val);
+    echo var_dump($res);
+
+    #do whatever to place the username in the table
+    #do whatever to put encrypted password in DB
+    #i changed someting
+    return $response;
+});

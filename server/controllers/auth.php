c0d912b6e491cd55f0c8758834e9808571c717df
TrevinN <tnance541@gmail.com>
2018-02-15 18:14:13 -0600

    changed some stuff in auth and users and changed user model

--- server/controllers/auth.php
+++ server/controllers/auth.php
@@ -12,6 +12,9 @@ $app->post('/api/login', function (Request $request, Response $response, array $
     $email = $body["email"];
     $userPassword = hash('sha256', $body['password']);
 
+    $result = verifyBody($body);
+    if ($result != true) return false; //change to bad username or password error
+
     echo $email;
     echo $userPassword;
 
@@ -20,10 +23,31 @@ $app->post('/api/login', function (Request $request, Response $response, array $
     $userdump = json_encode($user);
     echo var_dump($userdump);
 
-    if ($user->password == $userPassword) {
-        echo "Success";
+    if ($user->password != $userPassword) {
+        return false; //switch to bad username or password error code
     }
+
     $response->getBody()->write(User::all());
 
-    return $response.json_encode;
+    echo $user("remember_token");
+    $token = hash('sha256', random_int(0,10000));
+    $user["remember_token"] = $token;
+    $user->save();
+    echo var_dumpI($user);
+
+    return array(
+        "email" => $email,
+        "token" => $token
+    );
 });
+
+function verifyBody($body){
+    if ($body["email"] == NULL){
+        return false; //return bad email code
+    }
+    if ($body["password"] == NULL){
+        return false; //return bad password code
+    }
+    return true;
+}
+
--- server/controllers/users.php
+++ server/controllers/users.php
@@ -2,6 +2,7 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Models\User;
 
 $app->get('/api/user/{id}', function (Request $request, Response $response, array $args) {
     $response->getBody()->write("get user");
@@ -17,20 +18,35 @@ $app->post('/api/user', function (Request $request, Response $response, array $a
     
     #user must have timezone ID made in table
     $body = $request->getParsedBody();
-    $username = $body['username'];
+    $email = $body['email'];
+    $name = $body['name'];
     #this line gets the password from args and encrypts it to store inDB
     $userPassword = hash('sha256', $body['password']);
     $user = new User;
-
+    
+    $result = verifyBody($body);
+    if ($result != true) return false; //return whatever error is given by vB
+    
+    $user['email'] = $email;
+    $user['name'] = $name;
+    $user['password'] = $userPassword;
 
     $res = $user->save();
-    echo var_dump($val);
+
     echo var_dump($res);
 
-
-
-    #do whatever to place the username in the table
-    #do whatever to put encrypted password in DB
-    #i changed someting
     return $response;
 });
+
+function verifyBody($body){
+    if ($body["email"] == NULL){
+        return false; //return bad email code
+    }
+    if ($body["password"] == NULL){
+        return false; //return bad password code
+    }
+    if ($body["name"] == NULL) {
+        return false; //return bad name code
+    }
+    return true;
+}

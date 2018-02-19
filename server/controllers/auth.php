4ff904d349ea0714a1e4b9911e19757ba6a364b0
TrevinN <tnance541@gmail.com>
2018-02-19 13:00:13 -0600

    changed bad login to return errors

--- server/controllers/auth.php
+++ server/controllers/auth.php
@@ -11,25 +11,25 @@ $app->post('/api/login', function (Request $request, Response $response, array $
     $body = $request->getParsedBody();
     #encrypts the inputted password to compare with the stored one
     $email = $body["email"];
+    if ($body['password'] == NULL){
+        $response->withStatus(400)->withHeader('Content-Type', 'text/html')->write('Password is empty');        
+    } return $response;
     $userPassword = hash('sha256', $body['password']);
 
-    // $result = verifyBody($body);
-    // if ($result != true) return $result; //change to bad username or password error
-// echo $email;
-  //  echo $userPassword;
+    if ($email == NULL) {
+        $response->withStatus(400)->withHeader('Content-Type', 'text/html')->write('Email is empty');        
+        return $response; 
+    }
 
     $user = User::where('email', '=', $email)->first();
     //$userdump = json_encode($user);
     //echo var_dump($userdump);
 
     if ($user->password != $userPassword) {
-        $response->withStatus(500)->withHeader('Content-Type', 'text/html')->write('Invalid Username or Password');
-        return $respone; //switch to bad username or password error code
+        $response->withStatus(400)->withHeader('Content-Type', 'text/html')->write('Invalid Username or Password');
+        return $response; //switch to bad username or password error code
     }
 
-    // $response->getBody()->write(User::all());
-
-    // echo $user("remember_token");
     $token = hash('sha256', random_int(0,10000));//creates a random token to be stored and given to the user
     $user["remember_token"] = $token;
     $user->save();//saves the newly updated user info to the db
@@ -45,8 +45,8 @@ $app->post('/api/login', function (Request $request, Response $response, array $
     $response->write($UserToken);
 
 
-    $testvar = validateUser($UserToken);
-    echo($testvar);
+    // $testvar = validateUser($UserToken);
+    // echo($testvar);
     return $response; #returns a unique token consisting of an email and random token
 });
 

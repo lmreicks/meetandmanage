13bf82aeda79fa009cbc9a7584ea47ab9c6f6e06
TrevinN <tnance541@gmail.com>
2018-02-19 13:29:56 -0600

    commented on functions, changed errors, cleaned up stuff

--- server/controllers/auth.php
+++ server/controllers/auth.php
@@ -4,42 +4,40 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use Models\User;
 
+// takes in 'email', 'password' in $body
+// returns 400 error 'Invalid Username or Password' if bad login
+// returns authorization token otherwise  
 
 $app->post('/api/login', function (Request $request, Response $response, array $args) {
 
-    require "logic/validator.php";
     $body = $request->getParsedBody();
     
     #encrypts the inputted password to compare with the stored one
     $email = $body["email"];
 
     if ($body['password'] == NULL){
-        $response->withStatus(400)->withHeader('Content-Type', 'text/html')->write('Password is empty'); 
+        $response->withStatus(400)->withHeader('Content-Type', 'text/html')->write('Invalid Username or Password'); 
         return $response;       
     } 
 
     $userPassword = hash('sha256', $body['password']);
 
     if ($email == NULL) {
-        $response->withStatus(400)->withHeader('Content-Type', 'text/html')->write('Email is empty');        
+        $response->withStatus(400)->withHeader('Content-Type', 'text/html')->write('Invalid Username or Password');        
         return $response; 
     }
 
     $user = User::where('email', '=', $email)->first();
-    //$userdump = json_encode($user);
-    //echo var_dump($userdump);
 
-    if ($user->password != $userPassword) {
+    if ($user->password != $userPassword || $user == NULL) {
         $response->withStatus(400)->withHeader('Content-Type', 'text/html')->write('Invalid Username or Password');
-        return $response; //switch to bad username or password error code
+        return $response; 
     }
 
     $token = hash('sha256', random_int(0,10000));//creates a random token to be stored and given to the user
     $user["remember_token"] = $token;
     $user->save();//saves the newly updated user info to the db
-    //echo var_dump($user);
 
-    echo("success");
     $UserToken = array(
         "email" => $email,
         "remember_token" => $token
@@ -48,9 +46,6 @@ $app->post('/api/login', function (Request $request, Response $response, array $
     $UserToken = json_encode($UserToken);
     $response->write($UserToken);
 
-
-    // $testvar = validateUser($UserToken);
-    // echo($testvar);
     return $response; #returns a unique token consisting of an email and random token
 });
 
